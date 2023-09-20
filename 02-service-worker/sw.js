b11de853104e7d6debf3e642c34309e81447c942
juanPabloCesarini <juanpcesarini@hotmail.com>
2023-09-20 08:02:07 -0300

    fin seccion 4

--- 02-service-worker/sw.js
+++ 02-service-worker/sw.js
@@ -1,11 +1,16 @@
 self.addEventListener('fetch', event => {
    const respuesta = fetch(event.request)
       .then(resp => {
-         if (resp.ok) {
+       /*   if (resp.ok) {
             return resp;
          } else {
+            
             return fetch('img/main.jpg');
-         }
+         } */
+
+         // con operador ternario//
+         
+         return resp.ok ? resp : fetch('img/main.jpg');
 
       })
    event.respondWith(respuesta);
